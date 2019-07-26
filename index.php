4b4b9a134070c1c3d566666936151cf11f46a4a4
Yu Hsien Chiang <samy9ch@gmail.com>
2019-07-26 17:06:10 +0800

    ready to deploy

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once("home.html"); ?>
+/*
+ * File              : index.php
+ * @author           : Yu Hsien Chiang <samy9ch@gmail.com>
+ * Date              : 26.07.19 Fri 17:03:29
+ * Last Modified Date: 26.07.19 Fri 17:03:29
+ * Last Modified By  : Yu Hsien Chiang <samy9ch@gmail.com>
+ */
