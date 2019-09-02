b60c7ef06eeebcdc224299ac66467e87dceded6c
Yu Hsien Chiang <samy9ch@gmail.com>
2019-09-02 16:52:30 +0800

    first test

--- scripts/lazyLoad.js
+++ scripts/lazyLoad.js
@@ -2,13 +2,8 @@ var flag = 0;
 
 function lazyLoad() {
     var lazy = document.getElementsByClassName('lazy');
-    if (flag == 0) {
-        for (var k = 0; k < 3; k++) {
-            lazy[k].src =lazy[k].getAttribute('data-src');
-        }
-        flag = 1;
-    }
-    for (var i = 3; i < lazy.length; i++) {
+    
+    for (var i = 0; i < lazy.length; i++) {
         if(isInViewport(lazy[i])){
             lazy[i].src =lazy[i].getAttribute('data-src');
         }
